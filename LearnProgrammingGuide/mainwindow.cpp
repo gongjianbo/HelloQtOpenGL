638cd3042a643ddee2fbf0de064301e364b12009
gongjianbo <654344883@qq.com>
2020-08-02 22:14:33 +0800

    增加OpenGL编程指南的学习目录

---
+++ LearnProgrammingGuide/mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QMenu>
+#include <QAction>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    currentWidget=ui->label;
+
+    //单元选择-第2单元
+    /*ui->menuUnit->addAction("Unit2",[this]{
+        setWindowTitle("Unit2");
+        if(currentWidget){
+            currentWidget->deleteLater();
+        }
+        currentWidget=new Unit2Demo(this);
+        ui->verticalLayout->addWidget(currentWidget);
+    });*/
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
